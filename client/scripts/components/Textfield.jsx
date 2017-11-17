6242b80ed74358236f591db9f6b800813ffd4077
Jill Magsaysay <jcmagsay@gmail.com>
2017-11-17 02:44:19 -0700

    textfield styles

--- client/scripts/components/Textfield.jsx
+++ client/scripts/components/Textfield.jsx
@@ -1,12 +1,19 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 import Textfield from 'components/textfield';
+import styles from 'styles/components/textfield';
 
 export default class TodoList extends Component {
   constructor() {
     super();
 
+    this.state = {
+      focused: false
+    };
+
     this.handleChange = this.handleChange.bind(this);
+    this.handleFocus = this.handleFocus.bind(this);
+    this.handleBlur = this.handleBlur.bind(this);
   }
 
   handleChange(e) {
@@ -15,19 +22,34 @@ export default class TodoList extends Component {
     this.props.textChanged(val);
   }
 
+  handleFocus(e) {
+    this.setState({ focused: true });
+  }
+
+  handleBlur(e) {
+    this.setState({ focused: false });
+  }
+
   render() {
+    const focusClass = !this.state.focused ? styles.base : `${styles.base} ${styles.base}--focused`;
     return(
-      <fieldset>
-        <label htmlFor={this.props.id}>
-          {this.props.label}
-        </label>
+      <fieldset className={focusClass}>
         <input
           id={this.props.id}
+          className={styles.input}
           type={this.props.type}
           value={this.props.value}
           onChange={this.handleChange}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur}
         />
-        <span className={!this.props.error ? 'utilities__hidden': ''}>Error: can't be blank</span>
+        <label
+          className={styles.label}
+          htmlFor={this.props.id}
+        >
+          {this.props.label}
+        </label>
+        <span className={!this.props.error ? `${styles.error} utilities__hidden`: styles.error}>Error: can't be blank</span>
       </fieldset>
     );
   }
